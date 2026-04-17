d20450b3879a48117d9c106b17c30aa33ac2a58a
AlimAli333848 <abdulaleemali333@gmail.com>
2026-04-18 00:03:36 +0530

    time stamp 28:05

---
+++ Chapter1.cpp
@@ -0,0 +1,21 @@
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<iostream>
+using namespace std;
+using namespace cv;
+
+
+
+void  main() {
+
+
+
+	// images processing importing 
+	string path = "Resources/test.png";
+	Mat img = imread(path);
+
+
+
+	
+}
