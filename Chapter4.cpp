65ffe492449d5eadc81d180707c3c36b9f274dea
AlimAli333848 <abdulaleemali333@gmail.com>
2026-04-27 23:55:30 +0530

    27/4/26  1:12:12

--- Chapter4.cpp
+++ Chapter4.cpp
@@ -1,5 +1,5 @@
-#include<opencv2/imgcodecs.hpp>
-#include<opencv2/highgui.hpp>
+//#include<opencv2/imgcodecs.hpp>
+//#include<opencv2/highgui.hpp>
 #include<opencv2/imgproc.hpp>
 #include<iostream>
 using namespace std;
@@ -14,12 +14,17 @@ void  main() {
 
 
 	//  Blank images 
-	Mat img(512,512,CV_8UC3, Scalar(255,255,255)); //255 blue ,0, 267 pink
+	Mat img(512,512,CV_8UC3, Scalar(255,255,255)); //255 white ,0, 267 pink
 
 	circle(img, Point(256, 256), 155, Scalar(0, 69, 255),10);// scalar for colour defined // 512  half 256 // 10 for thickness
 	circle(img, Point(256, 256), 155, Scalar(0, 69, 255), FILLED);
 
-	rectangle(img, Point(130, 226), Point(382, 286), Scalar(255, 255, 255), 3); // 3 for thickness
+	rectangle(img, Point(130, 226), Point(382, 286), Scalar(255, 255, 255), FILLED); // 3 for thickness
+	line(img, Point(130, 296), Point(382, 296), Scalar(255, 255, 255),3);
+
+
+
+	putText(img, " computer visioon ",Point(137,262),FONT_HERSHEY_COMPLEX,1,Scalar(0,69,255),2);
 	
 
 	imshow("Images", img);
