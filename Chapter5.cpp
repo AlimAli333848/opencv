86bbf1581b684a57f1f710f339e9187d5b8ed5e9
AlimAli333848 <abdulaleemali333@gmail.com>
2026-05-26 23:30:13 +0530

    ts  1:16 35       26/5/26

--- Chapter5.cpp
+++ Chapter5.cpp
@@ -15,6 +15,12 @@ void  main() {
 
 	string path = "Resources/cards.jpg";
 	Mat img = imread(path);
+
+	Point2f src[4] = { {529,142},{771,190},{405,395},{674,457} };
+	Point2f src[4] = { {0.0f,0.0f},{w,0.0f},{405,395},{674,457} };
+
+
+
 	imshow("Image", img);
 	waitKey(0);	// to hold the image until we press a key
 
