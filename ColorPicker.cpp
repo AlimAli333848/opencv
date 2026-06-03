e71d97e09cb0f7c3a0ed653d0c49b507e4bcbad5
AlimAli333848 <abdulaleemali333@gmail.com>
2026-06-03 09:39:09 +0530

    ts 2   29  43                  3/6/26 virtual painter      code creatioon coontinue

--- ColorPicker.cpp
+++ ColorPicker.cpp
@@ -17,6 +17,33 @@ int main() {
 
 
 	namedWindow("Trackerbars", (640, 200));   //window creatted 
-	createTrackbar
+	createTrackbar("Hue Min", "Trackerbars", &hmin, 179);
+	createTrackbar("Hue Max", "Trackerbars", &hmax, 179);
+	createTrackbar("Sat Min", "Trackbars", &smin, 255);
+	createTrackbar("Sat Max", "Trackerbars", &max, 255);
+	createTrackbar("Val Min", "trackbars", &vmin, 255);
+	createTrackbar("Val Max", "Trackbars", &vmax, 255);
+	while (true) {
+		cap.read(img);
+		cvtColor(img, imgHSV, COLOR_BGR2HSV);
+	Scalar lower (hmin, smin, vmin);
+	Scalar  upper(hmax, smax, vmax);
+
+
+	inRange(imgHSV, lower, upper, mask);
+
+
+	cout << hmin << "," << smin << "," << vmin << "," << hmax << "," << smax << "," << vmax << endl;
+
+
+
+	imshow("Image", img);
+	imshow("mask", mask);
+
+
+	waitKey(1);
+	}
+
+
 
 }
