7c83e83e75c145e4b3138e6797b59f3fb68b6607
AlimAli333848 <abdulaleemali333@gmail.com>
2026-06-04 22:53:16 +0530

    4/6/26         ts 2  26   40 virtual painter cood creatioon  complete working

--- ColorPicker.cpp
+++ ColorPicker.cpp
@@ -1,49 +1,50 @@
-#include<opencv2/imgcodecs.hpp>
-#include<opencv2/highgui.hpp>
-#include<opencv2/imgproc.hpp>
-
-#include<iostream>
-using namespace std;
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
 using namespace cv;
+using namespace std;
 
 
-
-Mat imgHSV, mask, imgColor;
-int hmin = 0, smin = 110, vmin = 153;
-int hmax = 19, smax = 240, vmax = 255;
-VideoCapture cap(0);
-Mat img;
-int main() {
-
-
-	namedWindow("Trackerbars", (640, 200));   //window creatted 
-	createTrackbar("Hue Min", "Trackerbars", &hmin, 179);
-	createTrackbar("Hue Max", "Trackerbars", &hmax, 179);
-	createTrackbar("Sat Min", "Trackbars", &smin, 255);
-	createTrackbar("Sat Max", "Trackerbars", &max, 255);
-	createTrackbar("Val Min", "trackbars", &vmin, 255);
-	createTrackbar("Val Max", "Trackbars", &vmax, 255);
-	while (true) {
-		cap.read(img);
-		cvtColor(img, imgHSV, COLOR_BGR2HSV);
-	Scalar lower (hmin, smin, vmin);
-	Scalar  upper(hmax, smax, vmax);
+int main()
+{
+    VideoCapture cap(0);
+    Mat img;
+    Mat imgHSV, mask, imgColor;
+    int hmin = 0, smin = 0, vmin = 0;
+    int hmax = 179, smax = 255, vmax = 255;
+    
 
 
-	inRange(imgHSV, lower, upper, mask);
+    namedWindow("Trackbars", (640, 200)); // Create Window
+    createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+    createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+    createTrackbar("Sat Min", "Trackbars", &smin, 255);
+    createTrackbar("Sat Max", "Trackbars", &smax, 255);
+    createTrackbar("Val Min", "Trackbars", &vmin, 255);
+    createTrackbar("Val Max", "Trackbars", &vmax, 255);
 
+    while (true) {
 
-	cout << hmin << "," << smin << "," << vmin << "," << hmax << "," << smax << "," << vmax << endl;
+        cap.read(img);
+        cvtColor(img, imgHSV, COLOR_BGR2HSV);
 
+        Scalar lower(hmin, smin, vmin);
+        Scalar upper(hmax, smax, vmax);
 
-
-	imshow("Image", img);
-	imshow("mask", mask);
-
-
-	waitKey(1);
-	}
-
-
-
+        inRange(imgHSV, lower, upper, mask);
+        // hmin, smin, vmin, hmax, smax, vmax;
+        cout << hmin << "," << smin << "," << vmin << "," << hmax << "," << smax << "," << vmax << endl;
+        imshow("Image", img);
+        imshow("Mask", mask);
+        waitKey(1);
+    }
 }
+
+
+
+
+
+
+    
+    
