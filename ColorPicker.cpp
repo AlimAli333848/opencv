f0f2e177864a9c6212a2a8c93f1dfa53e9c56c6e
AlimAli333848 <abdulaleemali333@gmail.com>
2026-06-07 21:19:04 +0530

    7/6/26        virtual picker working  ts     2:39:03

--- ColorPicker.cpp
+++ ColorPicker.cpp
@@ -1,30 +1,30 @@
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <iostream>
-using namespace cv;
-using namespace std;
-
-
-int main()
-{
-    VideoCapture cap(0);
+//#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/highgui.hpp>
+//#include <opencv2/imgproc.hpp>
+//#include <iostream>
+//using namespace cv;
+//using namespace std;
+//
+//
+//int main()
+//{
+    /*VideoCapture cap(0);
     Mat img;
     Mat imgHSV, mask, imgColor;
     int hmin = 0, smin = 0, vmin = 0;
-    int hmax = 179, smax = 255, vmax = 255;
+    int hmax = 179, smax = 255, vmax = 255;*/
     
 
 
-    namedWindow("Trackbars", (640, 200)); // Create Window
-    createTrackbar("Hue Min", "Trackbars", &hmin, 179);
-    createTrackbar("Hue Max", "Trackbars", &hmax, 179);
-    createTrackbar("Sat Min", "Trackbars", &smin, 255);
-    createTrackbar("Sat Max", "Trackbars", &smax, 255);
-    createTrackbar("Val Min", "Trackbars", &vmin, 255);
-    createTrackbar("Val Max", "Trackbars", &vmax, 255);
+    //namedWindow("Trackbars", (640, 200)); // Create Window
+    //createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+    //createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+    //createTrackbar("Sat Min", "Trackbars", &smin, 255);
+    //createTrackbar("Sat Max", "Trackbars", &smax, 255);
+    //createTrackbar("Val Min", "Trackbars", &vmin, 255);
+    //createTrackbar("Val Max", "Trackbars", &vmax, 255);
 
-    while (true) {
+    /*while (true) {
 
         cap.read(img);
         cvtColor(img, imgHSV, COLOR_BGR2HSV);
@@ -33,7 +33,7 @@ int main()
         Scalar upper(hmax, smax, vmax);
 
         inRange(imgHSV, lower, upper, mask);
-        // hmin, smin, vmin, hmax, smax, vmax;
+         hmin, smin, vmin, hmax, smax, vmax;
         cout << hmin << "," << smin << "," << vmin << "," << hmax << "," << smax << "," << vmax << endl;
         imshow("Image", img);
         imshow("Mask", mask);
@@ -42,7 +42,7 @@ int main()
 }
 
 
-
+*/
 
 
 
--- Virtual Painter.cpp
+++ Virtual Painter.cpp
@@ -1,65 +1,60 @@
-//#include<opencv2/imgcodecs.hpp>
-//#include<opencv2/highgui.hpp>
-//#include<opencv2/imgproc.hpp>
-//#include<iostream>
-//using namespace std;
-//using namespace cv;
-//
-//
-//vector<vector<int>>mycolors{{},{},{},{}}
-//
-//
-//
-//
-//auto findColor(Mat img)
-//{
-//
-//
-//	cvtColor(img, imgHSV, COLOR_BGR2HSV);
-//	Scalar lower(hmin, smin, vmin);
-//	Scalar upper(hmax, smax, vmax);
-//	inRange(imgHSV, lower, upper, mask);      // one MASK=  ONE  Color     
-//	                                          colour  detectioon coode   logic undersstanding unkown
-//}
-//	
-//
-//	
-//
-//
-//
-//void  main() {
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// virtual painter 
-//
-//
-//
-//	 zero for laptop  
-//
-//	VideoCapture cap(0);
-//	Mat img;
-//
-//	while (true) {
-//		cap.read(img);
-//		findColor(img);
-//
-//
-//		imshow("Image", img);
-//		waitKey(20);
-//	}
-//
-//
-//
-//
-//}
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<iostream>
+using namespace std;
+using namespace cv;
+vector<vector<int>> mycolors{
+
+	{124,48,117,143,170,255},{68,72,156,102,126,255} };   //purple //green 
+
+
+	vector<Scalar>myColorValues{  {255,0,255},{0,255,0}}; //purple // green
+
+
+
+
+
+
+
+void findcolor(Mat img) {
+	Mat imgHSV;
+
+
+	cvtColor(img, imgHSV, COLOR_BGR2HSV);
+	for (int i = 0; i < mycolors.size(); i++) {
+
+
+
+		Scalar lower(mycolors[i][0], mycolors[i][1],mycolors[i][2]);
+		Scalar upper(mycolors[i][3], mycolors[i][4],mycolors[i][5]);
+		Mat mask;
+		inRange(imgHSV, lower, upper, mask);
+		imshow(to_string(i), mask);
+	}
+
+}
+
+// webcam
+
+void  main() {
+
+
+	// zero for laptop  
+
+	VideoCapture cap(0);
+	Mat img;
+
+	while (true) {
+		cap.read(img);
+		findcolor(img);
+
+
+		imshow("Image", img);
+		waitKey(1);
+	}
+
+
+
+
+}
