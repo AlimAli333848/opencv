7bff02f6f0a4f856851a7d33fee2b2547f0e1df5
AlimAli333848 <abdulaleemali333@gmail.com>
2026-07-02 22:18:42 +0530

    2/7/26 ts 3:10:35

--- Document Scanner .cpp
+++ Document Scanner .cpp
@@ -5,7 +5,31 @@
 using namespace std;
 using namespace cv;
 
-Mat imgOrginal, imgGray, imgCanny;
+Mat imgOrginal, imgGray, imgCanny, imgThre;
+Mat  imgErode, imgDil, imgBlur;
+
+
+Mat preProcessing(Mat img)
+{
+	cvtColor(img, imgGray, COLOR_BGR2GRAY);
+
+
+
+	GaussianBlur(imgGray, imgBlur, Size(7, 7), 5, 0);
+	Canny(imgBlur, imgCanny, 25, 75);  // egde detector
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 5));
+
+	dilate(imgCanny, imgDil, kernel);
+	//erode(imgDil, imgErode, kernel);
+
+	return imgDil;
+
+
+
+
+
+}
+
 
 void  main() {
 
@@ -14,10 +38,22 @@ void  main() {
 	// images processing importing
 
 	string path = "Resources/paper.jpg";
-	Mat img = imread(path);
+
+	Mat imgorginal = imread(path);
+
+	resize(imgOrginal, imgOrginal, Size(), 0.5, 0.5);
 
 
-	imshow("Image", img);
+	imgThre = preProcessing(imgorginal);
+	
+
+
+ 
+
+
+
+	imshow("Image", imgorginal);
+	imshow("Image dialatioon", imgThre);
 	waitKey(0);	// to hold the image until we press a key
 
 
